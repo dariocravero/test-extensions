ec9d9e6d109d89d576e36ebef969f0a95b901dd7
Darío Javier Cravero <dario@uxtemple.com>
2015-04-23 20:48:53 +0100

    js

--- code.js
+++ code.js
@@ -1,9 +1,9 @@
-import * as Blocks from 'panels-ui/blocks'
-import React from 'react'
+import * as Blocks from 'panels-ui/blocks';
+import React from 'react';
 
 export default class Block extends React.Component {
   render() {
-    let El = Blocks[this.props.block.type]
-    return <El {...this.props.block.toJS()}></El>
+    let El = Blocks[this.props.block.type];
+    return <El {...this.props.block.toJS()}></El>;
   }
 }
